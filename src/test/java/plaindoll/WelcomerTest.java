64ad692e7d1975d25ffab9e08d3f1201f7e729ef
Artyom M. <acider19@gmail.com>
2026-06-13 13:30:16 +0300

    Добавлены комментарии

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -6,34 +6,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WelcomerTest {
-	
-	private Welcomer welcomer = new Welcomer();
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
 
-	@Test
-	public void welcomerSaysWelcome() {
-		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
-	}
-	@Test
-	public void welcomerSaysFarewell() {
-		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
-	}
-	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
-	@Test
-	public void welcomerSaysSilver(){
-		assertThat(welcomer.sayNeedGold(), containsString("gold"));
-	}
-	@Test
-	public void welcomerSaysSomething(){
-		assertThat(welcomer.saySome(), containsString("something"));
-	}
-	@Test
-	public void welcomerSaySarov(){
-		assertThat(welcomer.saySarov(), containsString("hunter"));
-	}
+    private Welcomer welcomer = new Welcomer();
+
+    // Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
+    // https://t.me/notopsofficial
+
+    @Test
+    public void welcomerSaysWelcome() {
+        assertThat(welcomer.sayWelcome(), containsString("Welcome"));
+    }
+
+    @Test
+    public void welcomerSaysFarewell() {
+        assertThat(welcomer.sayFarewell(), containsString("Farewell"));
+    }
+
+    @Test
+    public void welcomerSaysHunter() {
+        assertThat(welcomer.sayWelcome(), containsString("hunter"));
+        assertThat(welcomer.sayFarewell(), containsString("hunter"));
+    }
+
+    @Test
+    public void welcomerSaysSilver() {
+        assertThat(welcomer.sayNeedGold(), containsString("gold"));
+    }
+
+    @Test
+    public void welcomerSaysSomething() {
+        assertThat(welcomer.saySome(), containsString("something"));
+    }
+
+    // добавил Муравский Артем FOPS-41
+    @Test
+    public void welcomerSaySarov() {
+        assertThat(welcomer.saySarov(), containsString("hunter"));
+    }
 }
